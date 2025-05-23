97b09ff94e80e8e96dfcdf05a16a83bf46f2ca9c
Dom Clayton <dominic.clayton@parkingeye.co.uk>
2025-05-23 14:54:13 +0100

    changes

--- web/config.sh
+++ web/config.sh
@@ -9,7 +9,13 @@ ufw --force enable
 
 # Update and install dependencies
 apt-get update
-apt-get install -y net-tools nginx nodejs npm
+apt-get install -y net-tools nginx curl gnupg2s
+
+# Get Node from Node Source PPA
+curl -fsSL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
+chmod +x nodesource_setup.sh
+./nodesource_setup.sh
+apt install npm
 
 # copy nginx site configuration into place
 cp /vagrant/web/nginx/sites-available/reactapp/reactapp /etc/nginx/sites-available/reactapp
@@ -19,6 +25,21 @@ mkdir -p /var/www/reactapp/html
 
 # Build node app
 cd /vagrant/web
-#npm run build
+
+# Install node modules
+npm install 
+
+npm run build
 
 # copy web content
+cp build /var/www/reactapp/html
+
+# Enable the site
+ln -s /etc/nginx/sites-available/reactapp /etc/nginx/sites-enabled/reactapp
+
+# Test
+RESULT=$(nginx -t)
+echo $RESULT
+
+# reload config
+nginx -s reload
