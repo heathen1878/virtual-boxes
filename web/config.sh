e78ba977d73fb721855d7485378aa3b21c354bd8
Dom Clayton <dominic.clayton@parkingeye.co.uk>
2025-05-23 14:01:27 +0100

    Added node package file, and npm package

--- web/config.sh
+++ web/config.sh
@@ -9,5 +9,16 @@ ufw --force enable
 
 # Update and install dependencies
 apt-get update
-apt-get install -y net-tools nginx nodejs
+apt-get install -y net-tools nginx nodejs npm
 
+# copy nginx site configuration into place
+cp /vagrant/web/nginx/sites-available/reactapp/reactapp /etc/nginx/sites-available/reactapp
+
+# Create directories for web app
+mkdir -p /var/www/reactapp/html
+
+# Build node app
+cd /vagrant/web/src
+npm run build
+
+# copy web content
