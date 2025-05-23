4c6c494cced78d5637c6aa5563adcdea4d40a4b5
Dom Clayton <dominic.clayton@parkingeye.co.uk>
2025-05-23 14:58:05 +0100

    reordered and cleaned up

--- web/config.sh
+++ web/config.sh
@@ -12,10 +12,10 @@ apt-get update
 apt-get install -y net-tools nginx curl gnupg2s
 
 # Get Node from Node Source PPA
-curl -fsSL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
+curl -fsSL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
 chmod +x nodesource_setup.sh
 ./nodesource_setup.sh
-apt install npm
+apt install nodejs
 
 # copy nginx site configuration into place
 cp /vagrant/web/nginx/sites-available/reactapp/reactapp /etc/nginx/sites-available/reactapp
@@ -23,16 +23,8 @@ cp /vagrant/web/nginx/sites-available/reactapp/reactapp /etc/nginx/sites-availab
 # Create directories for web app
 mkdir -p /var/www/reactapp/html
 
-# Build node app
-cd /vagrant/web
-
-# Install node modules
-npm install 
-
-npm run build
-
 # copy web content
-cp build /var/www/reactapp/html
+cp -r /vagrant/web/public/* /var/www/reactapp/html
 
 # Enable the site
 ln -s /etc/nginx/sites-available/reactapp /etc/nginx/sites-enabled/reactapp
